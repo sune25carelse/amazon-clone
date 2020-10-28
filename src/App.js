72c8830d83075e06a62c740c77f35e7bf00b6bf4
Sune Carelse <Sune.Carelse@hq.takealot.com>
2020-10-28 22:17:41 +0200

    updatded basket and checkout with total cost

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
 import Login from "./Login";
+import { auth } from "./firebase";
+import Checkout from "./Checkout";
 
 function App() {
+  
+  useEffect(() =>{
+    //will only runn once when the app component loads...
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>> ', authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatchEvent({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatchEvent({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
-    <div classname="app">
-      <Router>
+
+    <Router>
+      <div classname="app">
+        <Header />
         <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
+          <Route path="/login">
+           <Login />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 export default App;
