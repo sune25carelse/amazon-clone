cb0ec76b33d4e3eb20c3cf7422970bbbdab13d8a
Sune Carelse <Sune.Carelse@hq.takealot.com>
2020-10-29 23:16:39 +0200

    created checkout products & remove from basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+  const [{ basket, user }, dispacth] =useStateValue();
+
   return (
     <div className="checkout">
         <div className="checkout__left">
@@ -14,12 +18,17 @@ function Checkout() {
 
          <div>
             <h2 className="checkout__title">Your shopping Basket</h2> 
-              
-            {/* BasketItem */}
-            {/* BasketItem */}
-            {/* BasketItem */}
-            {/* BasketItem */}
-            {/* BasketItem */}
+           
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+            
          </div>
         </div>
         
